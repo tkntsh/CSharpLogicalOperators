2c8c54601c45b3e1eda4069faa889fffcb1de31f
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:34:45 +0200

    Create logic2.cs
    Write a statement to find if 100 is greater than 55.60 or 89 less than 100.
    Print to the Console.
    What is the answer?

---
+++ logic2.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if(100>55.60 || 89<100)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
