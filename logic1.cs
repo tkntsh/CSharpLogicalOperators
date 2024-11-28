0ac7ae2005eb0508c9614ade727ade032454895a
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:34:17 +0200

    Create logic1.cs
    Write a statement to find if 10 is greater than 4 and 4 is less than 1.
    Print to the Console.
    What is the answer?

---
+++ logic1.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if(10>4 && 4<1)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
