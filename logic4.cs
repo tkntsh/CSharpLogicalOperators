9366ddc118fb6388d5fdc001c7da04e23848143b
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:35:39 +0200

    Create logic4.cs
    Write a statement to find if 100 is less than 101 and 68 is equal to 33 multiplied by 2.
    Print to the Console.
    What is the answer?

---
+++ logic4.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if(100<101 && 68 == 33*2)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
