3f21e33ef988f1310e53bac4a15bcceacbd00525
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:35:11 +0200

    Create logic3.cs
    Write a statement to find if 50 is not equal to 55.60.
    Print to the Console.
    What is the answer?

---
+++ logic3.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if(50 == 55.60)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
